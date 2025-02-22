44f295c789620c267bf5641e5fa9d95d9cb4cc73
joefarrier <joefarrier@gmail.com>
2025-02-22 10:13:18 -0800

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,3 +1,5 @@
-export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+import React from 'react';
+
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return <input {...props} />;
 }
